974006163bcade556da6f5df950c50cc7c9537a0
Jimmy Ho <ho@jimmyho.com>
2016-07-14 23:33:06 +0100

    Bump version: 0.1.0 → 0.2.0

--- postcodes_io/__init__.py
+++ postcodes_io/__init__.py
@@ -3,6 +3,6 @@ from .resource import Postcodes
 
 __author__ = 'James Ho'
 __email__ = 'jho@neverlimited.uk'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 __all__ = ['Postcodes']
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='postcodes_io',
-    version='0.1.0',
+    version='0.2.0',
     description="Python client to connect to Postcodes.io API",
     long_description=readme + '\n\n' + history,
     author="James Ho",
